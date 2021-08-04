ef5309d52fc8c99d49b658593765820c9fa0a4fd
Begimai <begimai.akulova@gmail.com>
2021-08-05 02:01:43 +0300

    finished the project

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /* eslint-disable indent */
-const quizData = [
-    {
+const quizData = [{
         question: "Which is the smallest ocean in the world?",
         a: "Indian",
         b: "Pacific",
@@ -20,19 +19,19 @@ const quizData = [
     },
 
     {
-    question: "Dead sea is located between which two countries?",
-    a: "Jordan and Sudan",
-    b: "Jordan and Israel",
-    c: "Turkey and UAE",
-    d: "UAE and Egypt",
-    correct: "b"
+        question: "Dead sea is located between which two countries?",
+        a: "Jordan and Sudan",
+        b: "Jordan and Israel",
+        c: "Turkey and UAE",
+        d: "UAE and Egypt",
+        correct: "b"
     },
 
     {
         question: "Which country is known as playground of Europe?",
         a: "Austria",
         b: "Holland",
-        c: "Switzerland", 
+        c: "Switzerland",
         d: "Italy",
         correct: "c"
     }
@@ -40,26 +39,73 @@ const quizData = [
 
 ];
 
-const questionElem=document.querySelector("#question"),
-      a_text=document.querySelector("#a_text"),
-      b_text=document.querySelector("#b_text"),
-      c_text=document.querySelector("#c_text"),
-      d_text=document.querySelector("#d_text");
+const questionElem = document.querySelector("#question"),
+    a_text = document.querySelector("#a_text"),
+    b_text = document.querySelector("#b_text"),
+    c_text = document.querySelector("#c_text"),
+    d_text = document.querySelector("#d_text"),
+    submitBtn = document.querySelector("#submit"),
+    answerEls = document.querySelectorAll(".answer"),
+    quiz = document.querySelector("#quiz");
 
 
 
 
-      
-let currentQuiz=0;
+let currentQuiz = 0;
+let score = 0;
 loadQuiz();
 
-function loadQuiz(){
-    const currentQuizData=quizData[currentQuiz];
-    questionElem.innerText=currentQuizData.question;
-    a_text.innerText=currentQuizData.a;
-    b_text.innerText=currentQuizData.b;
-    c_text.innerText=currentQuizData.c;
-    d_text.innerText=currentQuizData.d;
 
-    currentQuiz++;
+function loadQuiz() {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionElem.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+
 }
+
+function getSelected() {
+    let answer = undefined;
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+
+    });
+
+    return answer;
+
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    const answer = getSelected();
+
+    if (answer) {
+       if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+
+        currentQuiz++;
+
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>You answered correctly to ${score} out of ${quizData.length} questions</h2>
+        <button onclick="location.reload()">Reload</button>`;
+
+        }
+    }
+
+});
